53d5777c314d0e6f81d2bbc6bedbf98c13c7d73e
Ammu <153814176+AnilKoli123@users.noreply.github.com>
2026-04-10 23:22:32 +0530

    run.py

--- run.py
+++ run.py
@@ -3,8 +3,10 @@ from app.models import db
 
 app = create_app()
 
+# Create DB tables
 with app.app_context():
     db.create_all()
 
+# Local run only
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
